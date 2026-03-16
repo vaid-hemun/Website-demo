47a490832b3af04cd0ecf7fcdd1e0ea51c621753
Hemun Vaid <youremail@gmail.com>
2026-03-16 12:36:44 +0530

    fix API endpoint

--- client/src/pages/Enquiry.jsx
+++ client/src/pages/Enquiry.jsx
@@ -24,7 +24,7 @@ export default function Enquiry() {
   e.preventDefault();
 
   try {
-    const response = await fetch("https://website-demo-eliu.onrender.com", {
+    const response = await fetch("https://website-demo-eliu.onrender.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
